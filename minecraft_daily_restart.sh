d5cd638726064b63066645b42570c7a4c680f9c7
Anto Prad <antonin.pradon@gmail.com>
2026-02-23 11:07:23 +0100

    Apply Aikar's flags

--- minecraft_daily_restart.sh
+++ minecraft_daily_restart.sh
@@ -8,7 +8,7 @@ MINECRAFT_SERVER="${MINECRAFT_SERVER:-false}"
 SERVER_DIR="/srv/minecraft"
 MCR_USER="minecraft"
 SCREEN_NAME="mc"
-START_CMD="java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC -jar fabric-server-mc.1.21.1-loader.0.18.0-launcher.1.1.0.jar nogui"
+START_CMD="java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar fabric-server-mc.1.21.1-loader.0.18.0-launcher.1.1.0.jar nogui"
 KILL_PATTERN="fabric-server-mc\.1\.21\.1-loader\.0\.18\.0-launcher\.1\.1\.0\.jar"
 
 log() { echo "$(date --iso-8601=seconds) $*"; }
